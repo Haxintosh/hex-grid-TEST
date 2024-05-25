f470100618f784bf4c71e9a38d68e29e14edd6ce
Geenk <goose@sillygoosy.ca>
2024-05-24 21:11:07 -0400

    dialogue done ish (css)

--- README.md
+++ README.md
@@ -30,3 +30,49 @@ cryptex.destroy(); // destroy
 
 cryptex.checkAnswer() // check if the answer is correct, true if correct, false if not
 ```
+## Dialogue
+```js
+import Dialogue from './Dialogue.js';
+const dialogueText = [
+    {
+        name: 'Flagellum Dei',
+        text: 'For I am Attila the Hun, the scourge of God. If you had not committed great sins, God would not have sent a punishment like me upon you.'
+    },
+    {
+        name: 'Flagellum Dei',
+        text: 'Shame on you for your despicable sins and your great wickedness. You have brought great suffering upon yourselves.'
+    },
+    {
+        name: 'Flagellum Dei',
+        text: 'Now eat this sucker!'
+    }
+]
+
+const Dialogue1 = new Dialogue(dialogueText, 40, false, 'audio/click.ogg', parent, () => {
+    console.log('done');
+    Dialogue1.destroy();
+});
+
+Dialogue1.startFromOrigin();
+```
+### Constructor
+```js
+new Dialogue(dialogueText, timePerLetter, autoSkip, srcToAudio, parentElement, callback); // Arr, int, bool, str, E, func
+
+dialogueText = [
+    {name: 'Name', text: 'Text'},
+    {name: 'Name', text: 'Text'}, //.. etc
+]
+```
+### Methods
+```js
+startFromOrigin(); // start the dialogue from the beginning
+
+stop(); // stop the dialogue
+
+resume(); // resume the dialogue where it stopped
+
+reset(); // reset the dialogue to the beginning of everything
+
+destroy(); // destroy the dialogue
+```
--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import './style.css'
-import Inventory, {Item, DropZone} from "./Inventory.js";
+import Dialogue from "./Dialogue.js";
 const parent = document.getElementById('Cryptex')
 
 // const electrical = new ElectricalPuzzle(parent, 500, 500, {
@@ -8,18 +8,39 @@ const parent = document.getElementById('Cryptex')
 //     bgColor: 'black'
 // });
 
-const inv = new Inventory(parent, {}, 5);
-const item1 = new Item({url: 'https://picsum.photos/50'}, 'item1', 'description', inv, 1);
-const item2 = new Item({url: 'https://picsum.photos/50'}, 'item2', 'description', inv, 1);
-const item3 = new Item({url: 'https://picsum.photos/50'}, 'item3', 'description', inv, 1);
-const item4 = new Item({url: 'https://picsum.photos/50'}, 'item4', 'description', inv, 2);
+// const inv = new Inventory(parent, {}, 5);
+// const item1 = new Item({url: 'https://picsum.photos/50'}, 'item1', 'description', inv, 1);
+// const item2 = new Item({url: 'https://picsum.photos/50'}, 'item2', 'description', inv, 1);
+// const item3 = new Item({url: 'https://picsum.photos/50'}, 'item3', 'description', inv, 1);
+// const item4 = new Item({url: 'https://picsum.photos/50'}, 'item4', 'description', inv, 2);
+//
+// inv.addItem(item1);
+// inv.addItem(item2);
+// inv.addItem(item3);
+// inv.addItem(item4);
+//
+// inv.removeItem(item2);
+//
+// const dropZone = new DropZone(100, 100, 500, 500, parent);
+// const dropZone2 = new DropZone(150, 150, 200, 300, parent);
 
-inv.addItem(item1);
-inv.addItem(item2);
-inv.addItem(item3);
-inv.addItem(item4);
+const dialogueText = [
+    {
+        name: 'Flagellum Dei',
+        text: 'For I am Attila the Hun, the scourge of God. If you had not committed great sins, God would not have sent a punishment like me upon you.'
+    },
+    {
+        name: 'Flagellum Dei',
+        text: 'Shame on you for your despicable sins and your great wickedness. You have brought great suffering upon yourselves.'
+    },
+    {
+        name: 'Flagellum Dei',
+        text: 'Now eat this sucker!'
+    }
+]
+const Dialogue1 = new Dialogue(dialogueText, 40, false, 'audio/click.ogg', parent, () => {
+    console.log('done');
+    Dialogue1.destroy();
+});
 
-inv.removeItem(item2);
-
-const dropZone = new DropZone(100, 100, 500, 500, parent);
-const dropZone2 = new DropZone(150, 150, 200, 300, parent);
+Dialogue1.startFromOrigin();
